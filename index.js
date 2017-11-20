1e0ea97b17444857fe395577496be7989b7ca6f7
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-20 18:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function findMatching (drivers, string) {
   const matchingDrivers = drivers.filter( function(driver) { return driver.toLowerCase() === string.toLowerCase() } );
   return matchingDrivers;
 }
+
+function fuzzyMatch (drivers, string) {
+  const matchingDrivers = drivers.filter( function(driver) { if (driver[0] && driver[1] === string) { return driver}})
+  return matchingDrivers;
+}
