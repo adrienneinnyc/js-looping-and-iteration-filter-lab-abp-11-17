03d06c0ad6ee04ef9e444db5a76c4e00c5517957
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-20 18:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function findMatching (drivers, string) {
-  drivers.filter( function (driver) { return driver.toLowercase === `${string}.toLowercase` } );
+  drivers.filter( function (driver) { return driver.toLowercase === `${string.toLowercase}` } );
 }
