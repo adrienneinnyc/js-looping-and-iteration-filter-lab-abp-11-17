04d322a3346110ff01d16d8efe8c3f287dc2df7d
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-20 18:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ function findMatching (drivers, string) {
   const matchingDrivers = drivers.filter( function(driver) { return driver.toLowerCase() === string.toLowerCase() } );
   return matchingDrivers;
 }
-
-findMatching(drivers, string);
